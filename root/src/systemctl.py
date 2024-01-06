f4a810f032e5e605f5efd96addbff9191766de49
TPlaysNow <tassopsaltakis@me.com>
2024-01-05 22:44:01 -0700

    system ctl start for #15
    Directories and starting files for systemctl system.

---
+++ root/src/systemctl.py
@@ -0,0 +1,5 @@
+#System services running 24/7 will be maanged from here
+
+#startup - services that are run on start up are pulled from the startup_services.txt file will be called on system boot. 
+
+#
